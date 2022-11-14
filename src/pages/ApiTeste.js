0bb01abfab2e38049acf9b474531c3c3b43e9496
karen okasaki <oks.karen@gmail.com>
2022-11-14 14:43:24 -0300

    axios & useEffect

--- src/pages/ApiTeste.js
+++ src/pages/ApiTeste.js
@@ -1,24 +1,58 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ApiTeste() {
+  const [alunos, setAlunos] = useState([]);
+  const [reload, setReload] = useState(false);
 
-    //fetch -> api do navegador -> requisições http (get, put, pacth, delete, post) -> axios
-    //promisses -> async/await
+  //fetch -> api do navegador -> requisições http (get, put, pacth, delete, post) -> axios
+  //promisses -> async/await
 
-    //useEffect()
-        // array de dependencias 
-            // [] -> vai rodar APENAS uma vez -> quando o componente/pagina for carregado
-            // [state] -> toda vez que esse state MUDAR DE VALOR -> o código dentro do useEffect roda novamente.
+  //useEffect()
+  // array de dependencias
+  // [] -> vai rodar APENAS uma vez -> quando o componente/pagina for carregado
+  // [state] -> toda vez que esse state MUDAR DE VALOR -> o código dentro do useEffect roda novamente.
 
+  useEffect(() => {
+    async function fetchStudents() {
+      const response = await axios.get(
+        "https://ironrest.herokuapp.com/enap-teste"
+      );
+      setAlunos(response.data);
+    }
 
-    useEffect(() => {}, [])
+    fetchStudents();
+  }, [reload]);
 
+  function handleReload() {
+    setReload(!reload);
+  }
 
-    return (
-        <div>
-            <h1>Aqui é a página que vamos mostrar a nossa API</h1>
-        </div>
-    )
+  console.log(alunos);
+
+  return (
+    <div>
+      <h1>Aqui é a página que vamos mostrar a nossa API</h1>
+
+      <form>
+        <input type="text" placeholder="Escreva seu nome" />
+        <input type="text" placeholder="Qual é a sua turma?" />
+        <button>Salvar aluno</button>
+      </form>
+
+      <button onClick={handleReload}>Recarregar api!!</button>
+
+      {alunos.map((aluno) => {
+        return (
+          <div key={aluno._id}>
+            <p>
+              {aluno.nome} - {aluno.turma}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ApiTeste
+export default ApiTeste;
